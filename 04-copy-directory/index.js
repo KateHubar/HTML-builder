4a17bb4f9ec74b52998d6d3c431e0d0023fb7c63
Kate Hubar <70864294+KateHubar@users.noreply.github.com>
2022-05-22 14:48:37 +0300

    feat: implemented task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const {copyFile, mkdir, readdir} = fs.promises;
+const path = require('path');
 
+const [,,basePath = 'files', distPath = 'files-copy' ] = process.argv;
+
+(async () => {
+  const dist = path.normalize(`${__dirname}/${distPath}`);
+  const src = path.normalize(`${__dirname}/${basePath}`);
+ 
+  const processDirItemsAndCopy = async (src, dist) => {
+    await mkdir(dist, { recursive: true });
+    const dirItems = await readdir(src, { withFileTypes: true, });
+    
+    dirItems.forEach(async (item) => {
+      const itemSrc = path.normalize(`${src}/${item.name}`);
+      const itemDist = path.normalize(`${dist}/${item.name}`);
+      if(item.isFile()) {
+        await copyFile(itemSrc, itemDist);
+      } else {
+        await processDirItemsAndCopy(itemSrc, itemDist);
+      }
+    });
+  };
+
+  await processDirItemsAndCopy(src, dist);
+
+  console.log('Сopying completed!');
+})();
